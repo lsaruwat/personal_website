c15d78d7afa30c68e82c5913932cdaff3d77b215
Logan Saruwatari <lsaruwatari@gmail.com>
2018-01-21 23:10:02 -0700

    changed php file io to hopefully work

--- follow.php
+++ follow.php
@@ -52,9 +52,8 @@ $current = file_get_contents($file);
 // Append a new person to the file
 $current .= $logdetails;
 // Write the contents back to the file
-file_put_contents($file, $current);
+$myfile = file_put_contents('visitors.log', $current.PHP_EOL , FILE_APPEND | LOCK_EX);
 
-fclose($fp);
 ?>
 <!DOCTYPE html>
 <html lang="en">
