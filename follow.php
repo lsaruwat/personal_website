924041c3357a4511d7322a3a8e8a7a8f7a292c34
Logan Saruwatari <lsaruwatari@gmail.com>
2018-01-21 22:25:01 -0700

    changed logging logic to fix bug

--- follow.php
+++ follow.php
@@ -1,6 +1,13 @@
 <?php
-$line = date('Y-m-d H:i:s') . " - $_SERVER[REMOTE_ADDR]";
-file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
+$logfile= 'visitors.log';
+$IP = $_SERVER['REMOTE_ADDR'];
+$logdetails= date("F j, Y, g:i a") . ': ' . '<a href=http://dnsstuff.com/tools/city.ch?ip='.$_SERVER['REMOTE_ADDR'].' target=_blank>'.$_SERVER['REMOTE_ADDR'].'</a>';
+
+// open the file for reading and writing
+$fp = fopen($logfile, "r+");
+// write out new log entry to the beginning of the file
+fwrite($fp, $logdetails, strlen($logdetails));
+fclose($fp);
 ?>
 <!DOCTYPE html>
 <html lang="en">
